fd63927011d11f92cdd212ec5f016ac280a0372c
Alphakyl <windowskeh@gmail.com>
2020-10-07 17:14:11 -0600

    Updated filter script, now works

--- scripts/filter.sh
+++ scripts/filter.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-files=/home/kyle/data/radar/icra/*
+files=/home/kyle/data/radar/icra/icra2021_test_data/ICRA2021_test_data/*
 for file in $files
 do
     echo "Existing file:"
-    echo ${file%.bag}
+    echo ${file}
     echo "New file:"
-    echo ${file%_no_tfs.bag}
+    echo ${file%.bag}_no_tf.bag
     echo "Filter:"
-    rosbag filter ${file%.bag} ${file%_no_tfs.bag} "topic == 'imu_in/data' or topic == 'imu_out/data' or topic == 'os1_cloud_node/imu' or topic=='os1_cloud_node/points'"
+    rosbag filter ${file} ${file%.bag}_no_tfs.bag "topic == '/imu_in/data' or topic == '/imu_out/data' or topic == '/os1_cloud_node/imu' or topic=='/os1_cloud_node/points'"
     echo "Filtering complete."
 done
 echo "Done"
