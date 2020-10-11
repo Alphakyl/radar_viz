d03ed6976af1d55774d274d587e993525500ac61
Alphakyl <windowskeh@gmail.com>
2020-10-11 15:42:16 -0600

    Rewrote odom publisher in CPP for convenience

--- scripts/posegraph2odom.py
+++ scripts/posegraph2odom.py
@@ -7,27 +7,35 @@ from std_msgs.msg import *
 
 class ODOM:
     def __init__(self):
+        print "odom_initialized"
         self.world_frame_id = "world"
         self.base_frame_id = "imu_link_enu"
         
-        self.sub=rospy.Subscriber('pose_graph_listener', pose_graph_msgs.PoseGraph, pose_graph_cb)
+
+        print "publisher_created"
         self.pub=rospy.Publisher('pose_graph_odom', Odometry, queue_size=10)
+        print "subscriber created"
+        
+        self.sub=rospy.Subscriber('/pose_graph', PoseGraph, self.pose_graph_cb)
     
     def pose_graph_cb(self, pgraph):
-        self.pose_graph_length = pgraph.size()
+        print "Entered Callback"
+        self.pose_graph_length = len(pgraph.poseGraph)
         print self.pose_graph_length
         for i in range(self.pose_graph_length):
             self.odom=Odometry()
             self.odom.child_frame_id=self.base_frame_id
-            self.odom.header=pgraph[i].header
+            self.odom.header=pgraph.poseGraph[i].header
             self.odom.header.frame_id=self.world_frame_id
-            self.odom.pose.pose=pgraph[i].pose
-            self.pub.publish(self.odom)
-            # rospy.sleep(rospy.Rate(20))
+            self.odom.pose.pose=pgraph.poseGraph[i].pose
+            self.pub.Publish(self.odom)
+            rospy.sleep(rospy.Rate(20))
 
 def main():
     rospy.init_node('posegraph2odom')
     create_odom=ODOM()
+    while not rospy.is_shutdown():
+        rospy.spin()
 
 if __name__ == '__main__':
     main()        
